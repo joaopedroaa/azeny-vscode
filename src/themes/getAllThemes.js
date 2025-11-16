fe98e3f57a647c6d4ebcc511619c5e88d07c6956
João Pedro Alvarenga <joaopedroaats@pm.me>
2025-11-15 21:52:00 -0300

    theme: create basic theme inveny config

--- src/themes/getAllThemes.js
+++ src/themes/getAllThemes.js
@@ -1,9 +1,11 @@
 import azenyTheme from './azeny.js';
+import azenyInvenyTheme from './azeny-inveny.js';
 import azenyOkanoTheme from './azeny-okano.js';
 import azenyCutimaruTheme from './azeny-cutimaru.js';
 
 export default {
   azenyTheme,
+  azenyInvenyTheme,
   azenyOkanoTheme,
   azenyCutimaruTheme,
 };
